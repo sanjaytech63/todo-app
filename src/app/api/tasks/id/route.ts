611439d720c85a5816aa43b7f439492eb514c445
Sanjay <sanjaytech63@gmail.com>
2025-08-03 19:16:30 +0530

    add auth

--- src/app/api/tasks/id/route.ts
+++ src/app/api/tasks/id/route.ts
@@ -1,39 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/app/lib/db/connect";
 import Task from "@/app/models/Task";
-import { CreateTaskSchema } from "@/app/lib/validations/task.schema";
-
-export async function GET() {
-  try {
-    await dbConnect();
-
-    const tasks = await Task.find().sort({ createdAt: -1 });
-    return NextResponse.json({ success: true, tasks });
-  } catch (error: any) {
-    return NextResponse.json(
-      { success: false, error: error?.message || "Internal Server Error" },
-      { status: 500 }
-    );
-  }
-}
-  
-// POST /api/task
-export async function POST(req: Request) {
-  try {
-    await dbConnect();
-
-    const body = await req.json();
-    const parsed = CreateTaskSchema.parse(body);
-
-    const newTask = await Task.create(parsed);
-    return NextResponse.json({ success: true, task: newTask }, { status: 201 });
-  } catch (error: any) {
-    return NextResponse.json(
-      { success: false, error: error?.message || "Internal Server Error" },
-      { status: 500 }
-    );
-  }
-}
 
 export async function PATCH(
   req: NextRequest,
