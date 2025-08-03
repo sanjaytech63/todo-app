c298579c0ac1c8d34a001e2276dc59c743c101db
Sanjay <sanjaytech63@gmail.com>
2025-08-03 20:16:53 +0530

    add task list api

--- src/app/api/tasks/[id}/route.ts
+++ src/app/api/tasks/[id}/route.ts
@@ -5,6 +5,36 @@ import Task from '@/app/models/Task';
 import mongoose from 'mongoose';
 
 
+import { CreateTaskSchema } from "@/app/lib/validations/task.schema";
+
+export async function GET() {
+    try {
+        await dbConnect();
+
+        const tasks = await Task.find().sort({ createdAt: -1 }); 
+        return NextResponse.json({ success: true, tasks });
+    } catch (e: unknown) {
+        const message = e instanceof Error ? e.message : 'Internal Server Error';
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
+// ✅ POST /api/task — Create a Task
+export async function POST(req: Request) {
+    try {
+        await dbConnect();
+
+        const body = await req.json();
+        const parsed = CreateTaskSchema.parse(body);
+
+        const newTask = await Task.create(parsed);
+        return NextResponse.json({ success: true, task: newTask }, { status: 201 });
+    } catch (e: unknown) {
+        const message = e instanceof Error ? e.message : 'Internal Server Error';
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
 
 // ✅ PATCH /api/tasks/[id] — Update a task
 export async function PATCH(
